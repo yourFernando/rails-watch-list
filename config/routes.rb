74c871065cb33a4cb8219aba250e31e2c6e5f55e
FERNANDO JOSE ALCANTARA CONDOR <alcantaracjose@gmail.com>
2024-08-31 16:36:59 -0500

    Root for deploy

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  # root to: "lists#index"
+  root to: "lists#index"
+  
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
